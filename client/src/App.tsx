1aea12a866816420cffb55ba2e471ead91cd8ac4
levykkn <levykkostya@gmail.com>
2024-03-01 20:29:18 +0000

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+
+
+const App: React.FC = () => {
+  return (
+    <ChakraProvider>
+    </ChakraProvider>
+  );
+};
+
+export default App;
