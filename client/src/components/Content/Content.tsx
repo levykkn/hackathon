87ab040781fa25389d3a5bf7397db894496df952
levykkn <levykkostya@gmail.com>
2024-03-02 14:58:23 +0000

    fixed get in touch button

--- client/src/components/Content/Content.tsx
+++ client/src/components/Content/Content.tsx
@@ -1,10 +1,10 @@
-import { Box, Button, Flex, Heading, Image } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Image, Text } from "@chakra-ui/react";
 import React from "react";
 
 const Content: React.FC = () => {
   return (
     <Flex h="90%" w="100%" justifyContent="center" align="center">
-      <Flex w="90%" h="80%">
+      <Flex w="90%">
         <Box>
           <Image 
           src="/main.jpeg" 
@@ -12,12 +12,12 @@ const Content: React.FC = () => {
           w="90%"
           />
         </Box>
-        <Flex direction="column" w="100%" p="5" justify="space-between">
+        <Flex direction="column" w="100%" justify="space-between">
           <Box>
-            <Heading as="h1" pb="5">
+            <Heading as="h1" pb="2">
               Fast charging batteries will change the future
             </Heading>
-            <Heading as="h6" size="xs" pb="5">
+            <Heading as="h6" size="xs" >
               Unlocking the potential of recharging batteries to deliver a
               sustainable and clean energy solution, powering a greener and
               brighter tomorrow
