4b00a4e2cd3ee858cd39f1b52f9c8da9820eccbf
levykkn <levykkostya@gmail.com>
2024-03-02 13:56:01 +0000

    added content component new features

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,12 +1,24 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Button, Flex, Link } from "@chakra-ui/react";
 import React from "react";
 import { Image } from "@chakra-ui/react";
+import Logo from "src/ui/Images/Logo";
 
 const Navbar: React.FC = () => {
   return (
     <Flex h="10%" w="100%">
-      <Flex justifyContent="space-between">
-        <Image src="/logo.svg" alt="Navbar Image" boxSize="200" />
+      <Flex justifyContent="space-between" w="90%" pt="6" align="center">
+        <Box w="50%">
+          <Image as={Logo} />
+        </Box>
+        <Flex justify="space-between" w="50%" align="center">
+          <Link>Home</Link>
+          <Link>About</Link>
+          <Link>Panels</Link>
+          <Link>Service</Link>
+          <Button colorScheme="teal" variant="outline">
+            Get in touch
+          </Button>
+        </Flex>
       </Flex>
     </Flex>
   );
---
+++ client/src/ui/Images/Logo.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Logo: React.FC = () => (
+    <img src="logo.svg" alt="logo" style={{height: "2.25em"}}  />
+)
+
+export default Logo;
