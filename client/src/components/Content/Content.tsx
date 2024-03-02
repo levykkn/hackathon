f30eb330685723b6ee890a08a1c2ec7d7781f91c
levykkn <levykkostya@gmail.com>
2024-03-02 14:14:40 +0000

    fixed image in content. positioned elements in content component

--- client/src/components/Content/Content.tsx
+++ client/src/components/Content/Content.tsx
@@ -3,19 +3,19 @@ import React from "react";
 
 const Content: React.FC = () => {
   return (
-    <Flex h="90%" w="100%">
-      <Flex w="100%" h="80%">
+    <Flex h="90%" w="100%" justifyContent="center" align="center">
+      <Flex w="90%" h="80%">
         <Box>
-          <Image src="main.jpeg"></Image>
+          <Image src="/main.jpeg" alt="Main Image" w="90%"/>
         </Box>
-        <Flex direction="column">
+        <Flex direction="column" w="100%">
           <Heading as="h1">
             Fast charging batteries will change the future
           </Heading>
           <Heading as="h6" size="xs">
             Unlocking the potential of recharging batteries to deliver a
             sustainable and clean energy solution, powering a greener and
-            brighter tommorow
+            brighter tomorrow
           </Heading>
           <Button colorScheme="teal" variant="outline">
             Get in touch
--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -7,8 +7,8 @@ const Navbar: React.FC = () => {
   return (
     <Flex h="10%" w="100%">
       <Flex justifyContent="space-between" w="90%" pt="6" align="center">
-        <Box w="50%">
-          <Image as={Logo} />
+        <Box w="30%">
+          <Image as={Logo} objectFit="contain" h="50%" w="10%" />
         </Box>
         <Flex justify="space-between" w="50%" align="center">
           <Link>Home</Link>
